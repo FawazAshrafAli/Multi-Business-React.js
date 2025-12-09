ddb30e06e5b684e0459935bd3e959d1a36c948ec
FawazAshrafAli <fawazashrafali@gmail.com>
2025-12-09 22:10:34 +0530

    Created router and component for order success  page.

---
+++ src/pages/success.js
@@ -0,0 +1,65 @@
+import SeoHead from "../../components/SeoHead";
+import Cart from "../../components/Cart";
+import axios from "axios";
+import OrderSuccess from "../../components/OrderSuccess";
+
+export default function successPage({
+  homeContent = {},
+  blogs = [],
+  user
+}) {
+  return (
+    <>
+      <SeoHead
+        meta_description={homeContent?.meta_description}
+        meta_title="Success! Order Placed!"
+        metaTags={[]}
+        blogs={blogs}
+        url="https://bzindia.in/success"
+      />      
+
+      <OrderSuccess user={user} />
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const cookie = req.headers.cookie || "";
+
+  try {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth_api/user/`, {
+      headers: { cookie },
+      withCredentials: true,
+    });
+
+    const user = res.data || null;
+
+    // 🔥 If no user → redirect BEFORE rendering
+    if (!user) {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        user,
+        homeContent: {},
+        blogs: [],
+      },
+    };
+  } catch (err) {
+    console.log("No User");
+
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+}
